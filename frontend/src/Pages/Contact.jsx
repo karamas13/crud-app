5f6555c923eb4e72a488424c03884aa0d05848c6
karamas <99655065+karamas13@users.noreply.github.com>
2023-07-04 18:44:01 +0300

    working on contact page

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,7 +1,28 @@
+import icon from '../images/git-icon.png';
+import facebook from '../images/facebook.png';
+import instagram from '../images/instagram.png';
+
 const Contact = () => {
     return ( 
-        <div className="text-center">
+        <div className="text-center " style={{height:'100%',minHeight:'100vh' }}>
             <h1>Contact</h1>
+            <div className='d-flex flex-row justify-content-center align-items-center p-5 ' >
+              <div className='d-flex flex-column m-5 p-3 border border-dark rounded border-3 '>
+                <img src={icon} height='150' width='auto'/>
+                <h3 className='mt-3'>GitHub</h3>
+
+              </div>
+              <div className='d-flex flex-column m-5 p-3 border border-dark rounded border-3 '>
+                <img src={instagram} height='150' width='auto'/>
+                <h3 className='mt-3'>Instagram</h3>
+
+              </div>
+              <div className='d-flex flex-column m-5 p-3 border border-dark rounded border-3 '>
+                <img src={facebook} height='150' width='auto'/>
+                <h3 className='mt-3'>Facebook</h3>
+
+              </div>
+            </div>
         </div>
      );
 }
